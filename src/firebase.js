feaef8c8e2bb604e2fbc607344716aeba53bbbad
Kapil Rana <kapsrana56@gmail.com>
2024-04-17 20:00:21 +0530

    chat-app-completed

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDIUgHPGZQQp1EW3QGs8orm0IQCCVjKRg8",
+  authDomain: "chat-app-9ebfa.firebaseapp.com",
+  projectId: "chat-app-9ebfa",
+  storageBucket: "chat-app-9ebfa.appspot.com",
+  messagingSenderId: "496623263851",
+  appId: "1:496623263851:web:51303c753afc45dd2d112e",
+  measurementId: "G-8RNV74NYRV",
+};
+
+
+export const app = initializeApp(firebaseConfig);
+
